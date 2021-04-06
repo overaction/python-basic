021ffa4826d89be047803ccee2200de742b9670d
overaction <kmc04@naver.com>
2021-04-06 15:24:14 +0900

    1-1. list

--- Python Syntax/4.py
+++ Python Syntax/4.py
@@ -2,7 +2,6 @@ a = "Hello, Dongguk!"
 print(a.upper())
 print(a.lower())
 print(a.replace('!', "~"))
-a = a.split(',')
-print(a[1])
+print(a.split(','))
 
 print(str(2017112181)+" 김만철")
---
+++ Python Syntax/5.py
@@ -0,0 +1,8 @@
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+
+print(myorder.format(quantity, itemno, price))
+
+print(str(2017112181)+" 김만철")
